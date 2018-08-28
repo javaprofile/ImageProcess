afde9741356e1e78e1cca6cb1499fa53cadd84ed
vipul <vipulreddy574@gmail.com>
2018-08-28 19:05:20 -0400

    Added configuration files

---
+++ src/main/java/com/imageprocess/config/AppConfigurationBean.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.imageprocess.config;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author vipul
+ *
+ */
+@Component
+public class AppConfigurationBean implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+	/**
+	 * 
+	 */
+	public AppConfigurationBean() {
+		// Default constructor
+	}
+
+	@Override
+	public void customize(ConfigurableServletWebServerFactory factory) {
+		factory.setContextPath("/imageprocess/rest");
+		
+	}
+
+}
