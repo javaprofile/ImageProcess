26813375bb7dfa769a25142c215d8fb63e73d0ef
vipul <vipulreddy574@gmail.com>
2018-08-28 22:14:22 -0400

    test

--- src/main/java/com/imageprocess/config/AppConfigurationBean.java
+++ src/main/java/com/imageprocess/config/AppConfigurationBean.java
@@ -23,7 +23,7 @@ public class AppConfigurationBean implements WebServerFactoryCustomizer<Configur
 
 	@Override
 	public void customize(ConfigurableServletWebServerFactory factory) {
-		factory.setContextPath("/imageprocess/rest");
+		//factory.setContextPath("/imageprocess/rest");
 		
 	}
 
